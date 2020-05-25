fbfde637ab42cf785f5f36c3ee9101407404caaa
525475256 <525475256@qq.com>
2020-05-25 15:43:53 +0800

    盘点流程修改

--- src/main/java/com/zoo/controller/erp/constant/InventoryCheckStatus.java
+++ src/main/java/com/zoo/controller/erp/constant/InventoryCheckStatus.java
@@ -3,8 +3,9 @@ package com.zoo.controller.erp.constant;
 public class InventoryCheckStatus {
 
 	public static final String WTJ = "WTJ";//未提交
-	public static final Object CW = "CW";//更新库存
-	public static final Object FINISHED = "FINISHED";
-	public static final Object ZGSH = "ZGSH";//主管审核
+	public static final String CW = "CW";//更新库存
+	public static final String FINISHED = "FINISHED";
+	public static final String ZGSH = "ZGSH";//主管审核
+	public static final String REJECT = "REJECT";//订单调整
 	
 }
--- src/main/java/com/zoo/listener/activiti/InventoryCheckGXKCReject.java
+++ src/main/java/com/zoo/listener/activiti/InventoryCheckGXKCReject.java
@@ -1,8 +1,6 @@
 package com.zoo.listener.activiti;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.activiti.engine.ProcessEngine;
@@ -16,15 +14,11 @@ import org.springframework.stereotype.Component;
 
 import com.zoo.controller.erp.constant.InventoryCheckStatus;
 import com.zoo.model.erp.inventorycheck.InventoryCheck;
-import com.zoo.model.system.position.Position;
-import com.zoo.model.system.user.SystemUser;
 import com.zoo.service.erp.inventorycheck.InventoryCheckService;
-import com.zoo.service.system.position.PositionService;
-import com.zoo.service.system.user.SystemUserService;
 import com.zoo.utils.ApplicationUtil;
 
 @Component("InventoryCheckGXKCReject")
-public class InventoryCheckGXKCReject implements ExecutionListener{
+public class InventoryCheckGXKCReject implements TaskListener{
 
 /**
 	 * 
@@ -33,40 +27,27 @@ public class InventoryCheckGXKCReject implements ExecutionListener{
 
 private ProcessEngine processEngine;
 	
-	private SystemUserService systemUserService;
-
-	private PositionService positionService;
 	
 	private InventoryCheckService inventoryCheckService;
 
 	@Override
-	public void notify(DelegateExecution execution) {
-		// TODO Auto-generated method stub
-		// TODO Auto-generated method stub
-				processEngine = (ProcessEngine) ApplicationUtil.getBean("processEngine");
-				RuntimeService runtimeService = processEngine.getRuntimeService();
-				inventoryCheckService = (InventoryCheckService)ApplicationUtil.getBean("inventoryCheckService");
-				
-				ProcessInstance result = runtimeService.createProcessInstanceQuery().processInstanceId(execution.getProcessInstanceId()).singleResult();
-				String key = result.getBusinessKey();
-				InventoryCheck ic = inventoryCheckService.getInventoryCheckById(key);
-				
-				Map<String,Object> condition = new HashMap<String,Object>();
-				
-				
-				condition = new HashMap<String,Object>();
-				condition.put("id", ic.getId());
-				condition.put("status", InventoryCheckStatus.ZGSH);
-				
-				inventoryCheckService.updateInventoryCheckStatus(condition);
-				
-		/*
-		 * List<Position> list = positionService.getPositionByCondition(condition);
-		 * List<String> groupIds = new ArrayList<String>();
-		 * 
-		 * for(Position position : list) { groupIds.add(position.getId()); }
-		 * delegateTask.addCandidateGroups(groupIds);
-		 */
+	public void notify(DelegateTask delegateTask) {
+			processEngine = (ProcessEngine) ApplicationUtil.getBean("processEngine");
+			RuntimeService runtimeService = processEngine.getRuntimeService();
+			inventoryCheckService = (InventoryCheckService)ApplicationUtil.getBean("inventoryCheckService");
+			
+			ProcessInstance result = runtimeService.createProcessInstanceQuery().processInstanceId(delegateTask.getProcessInstanceId()).singleResult();
+			String key = result.getBusinessKey();
+			InventoryCheck ic = inventoryCheckService.getInventoryCheckById(key);
+			
+			Map<String,Object> condition = new HashMap<String,Object>();
+			
+			
+			condition = new HashMap<String,Object>();
+			condition.put("id", ic.getId());
+			condition.put("status", InventoryCheckStatus.REJECT);
+			
+			inventoryCheckService.updateInventoryCheckStatus(condition);
 	}
 	
 	
